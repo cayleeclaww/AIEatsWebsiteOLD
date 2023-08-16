f21a019d12f1e48c0c1922b6c1c9be2625742e93
Caylee Claww <ninjahead@gmail.com>
2023-08-16 10:11:30 -0400

    adding nav bar links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,28 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <Link href="/about">About</Link>
+        </p><br />
+        <p>
+        <Link href="/bydisease">By Disease</Link>
+        </p><br />
+        <p>
+        <Link href="/bysymptom">By Symptom</Link>
+        </p><br />
+        <p>
+        <Link href="/foodsensitivites">Food Sensitivites</Link>
+        </p><br />
+        <p>
+        <Link href="/resources">Resources</Link>
+        </p><br />
+        
+        <Link href="/">Back to home</Link>
       </div>
 
       <div className={styles.center}>
---
+++ src/app/resources/supplements/page.tsx
@@ -0,0 +1,3 @@
+export default function Supplements() {
+    return <h1>Supplements</h1>;
+  }
