0533eb163b6464fb8cebf7693f2a6497eb824e18
Caylee Claww <ninjahead@gmail.com>
2023-08-17 21:29:24 -0400

    minor tweaks

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ import Head from 'next/head';
 export default function About() {
     return(
   <>
-  <h1>About Autoimmune Eats</h1>
+  <br/><h1><center>About Autoimmune Eats</center></h1>
   <p>
     Radical change is needed to improve the way we treat autoimmune disease. Autoimmune Eats is a resource to help
     guide this transformation. We support a revolution in care for the autoimmune community. </p>
@@ -23,28 +23,28 @@ export default function About() {
   <p>
     We are here to make you feel seen. We are here to make you feel heard. We are here to build community to birth
     a better future for people just like you.
-  </p>
+  </p><br/>
 
 <h2>Our Approach to Autoimmune Disease</h2>
   <p>
   We want less suffering from autoimmune disease in the world. Period. Our goal is to help you find more answers
   so you can live with less pain. Both western science and ancient indigenous wisdoms are our guiding lights. We support
-  both natural remedies and modern medications. 
-  </p>
+  the usage of both natural remedies and modern medications. 
+  </p><br/>
    
 
    <h3>Our Approach to Food</h3>
    <p>We believe in healing the gut, supporting natural detoxification pathways, identifying food sensitivies safely,
     and medications when necessary. We do not endorse diets, fat shaming, food sensitivity testing, fear of food, 
-    "detoxifying" outside of the body's natural pathways, or disordered eating. </p>
+    "detoxifying" outside of the body's natural pathways, or disordered eating. </p><br/>
 
     <h3>Our Approach to Neurodiversity</h3>
     <p>
       **Add statistic about % of AI dz and ND**. <br />
-      Neurodiversity is a beatiful thing - we believe in supporting,
+      Neurodiversity is a beautiful thing - we believe in supporting,
       uplifting, creating awareness for, plus reducing the pain and suffering of those who are neurodiverse and living 
       with autoimmune disease. We do not believe that neurodiversity is something that can or should be "cured".
-    </p>
+    </p><br/>
 
     <h3>Our Approach to Environmentalism</h3>
     <p>
