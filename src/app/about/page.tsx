945679aa80a2fddadbd851b76878ff0c9373b974
Caylee Claww <ninjahead@gmail.com>
2023-08-17 21:24:27 -0400

    first render of About section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,11 +10,48 @@ import Head from 'next/head';
 export default function About() {
     return(
   <>
-  <Head>
-  <title>About</title>
-  </Head>
   <h1>About Autoimmune Eats</h1>
+  <p>
+    Radical change is needed to improve the way we treat autoimmune disease. Autoimmune Eats is a resource to help
+    guide this transformation. We support a revolution in care for the autoimmune community. </p>
+    
+    <p>People living with
+    autoimmune disease face a wide set of physical, emotional, mental, and spiritual issues that are entirely
+    unaddressed in the modern treatment of autoimmune disease. 
+  </p>
+
+  <p>
+    We are here to make you feel seen. We are here to make you feel heard. We are here to build community to birth
+    a better future for people just like you.
+  </p>
+
+<h2>Our Approach to Autoimmune Disease</h2>
+  <p>
+  We want less suffering from autoimmune disease in the world. Period. Our goal is to help you find more answers
+  so you can live with less pain. Both western science and ancient indigenous wisdoms are our guiding lights. We support
+  both natural remedies and modern medications. 
+  </p>
    
+
+   <h3>Our Approach to Food</h3>
+   <p>We believe in healing the gut, supporting natural detoxification pathways, identifying food sensitivies safely,
+    and medications when necessary. We do not endorse diets, fat shaming, food sensitivity testing, fear of food, 
+    "detoxifying" outside of the body's natural pathways, or disordered eating. </p>
+
+    <h3>Our Approach to Neurodiversity</h3>
+    <p>
+      **Add statistic about % of AI dz and ND**. <br />
+      Neurodiversity is a beatiful thing - we believe in supporting,
+      uplifting, creating awareness for, plus reducing the pain and suffering of those who are neurodiverse and living 
+      with autoimmune disease. We do not believe that neurodiversity is something that can or should be "cured".
+    </p>
+
+    <h3>Our Approach to Environmentalism</h3>
+    <p>
+      Rates of autoimmune disease are on the rise. Why? We believe that increasing levels of toxic pollution in our air, 
+      water, food, and general environment are major contributors. We seek to provide inexpensive, realistic, and 
+      evidence-based solutions to help neurodiverse folks survive - and even thrive - in this reality.
+    </p>
   </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,8 @@ export default function Home() {
   return (
     <>
     <div className="container mx-auto px-4">
-    <h1>Hello word</h1>
-    <p>This is a content to make our page longer</p> <div className="w-full h-screen bg-green-300"></div> <p>
-    Lorem Ipsum is simply dummy text ...
-    </p> </div>
+    <h1>Main page</h1>
+    <p>This is where the main page content starts</p> </div>
     </>
     // <main className={styles.main}>
     //   <div className={styles.description}>
